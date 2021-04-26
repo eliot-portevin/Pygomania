124051995ff4e804ad5fcfddff7a4dfd68b8be67
MartinWho2 <martin.crettol@gmail.com>
2021-04-26 09:43:41 +0200

    Implementing Function to save place

--- spritesheet.py
+++ spritesheet.py
@@ -12,14 +12,17 @@ class Spritesheet:
             f.close()
         except :
             pass
-    def get_sprites(self,x,y,w,h):
-        sprite = pygame.Surface((w,h))
+    def get_sprites(self,x,y,w,h,x_o,y_o,w_o,h_o):
+        sprite = pygame.Surface((w_o,h_o))
         sprite.set_colorkey((0,0,0))
-        sprite.blit(self.sprite_sheet,(0,0),(x,y,w,h))
+        sprite.blit(self.sprite_sheet,(x_o,y_o),(x,y,w,h))
         return sprite
 
     def parse_sprites(self,name):
-        sprite = self.data['frames'][name]['frame']
-        x,y,w,h = sprite['x'],sprite['y'],sprite['w'],sprite['h']
-        image = self.get_sprites(x,y,w,h)
+        sprite = self.data['frames'][name]
+        in_image = sprite['frame']
+        original = sprite['spriteSourceSize']
+        x,y,w,h = in_image['x'],in_image['y'],in_image['w'],in_image['h']
+        x_o,y_o,w_o,h_o = original['x'],original['y'],original['w'],original['h']
+        image = self.get_sprites(x,y,w,h,x_o,y_o,w_o,h_o)
         return image
