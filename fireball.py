6be127e300c2aa5d0eda42fa8f715715a84839a1
eliot-portevin <eliotportevin@gmail.com>
2021-04-26 08:51:32 +0200

    adding files

---
+++ fireball.py
@@ -0,0 +1,31 @@
+import pygame, time, math
+
+class Fireball(pygame.sprite.Sprite):
+    def __init__(self,pos,dir):
+        super().__init__()
+        self.velocity = 300
+        self.image = pygame.transform.scale(pygame.image.load('media/Mage_animation/fireball.png'),(64,64))
+        # Position in x and y and direction in x (-1 for left and 1 for right)
+        self.position = pos
+        self.direction = dir
+        if self.direction == -1:
+            self.image = pygame.transform.flip(self.image,True,False)
+        self.rect = self.image.get_rect()
+        self.rect.x,self.rect.y = self.position
+        self.time = time.time()
+        self.begin_time = self.time
+        self.frames = 0
+
+    def move(self):
+        self.frames += 1
+        now = time.time()
+        dt = now - self.time
+        self.time = now
+        self.rect.x += self.velocity * self.direction * dt
+        if (math.fabs(self.rect.x-self.position[0]) > 1000) or (self.rect.x > 1920 or self.rect.right < 0):
+            self.remove()
+
+    def remove(self):
+        print(self.groups)
+        print(self.groups())
+        self.kill()
